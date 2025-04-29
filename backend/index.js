df7a2ca737a0728559663b0e053d2eeb95aff30a
Khrystyna <penyakkristi@gmail.com>
2025-04-29 22:55:47 +0300

    add gitignore

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
 });
