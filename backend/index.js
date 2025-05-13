cb2fc22271436044fe74fe067779bca6a8804306
Khrystyna <penyakkristi@gmail.com>
2025-05-13 21:48:41 +0300

    Add db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,14 @@
-const express = require("express");
+//const express = require("express");
+//const cors = require("cors");
+//const dotenv = require("dotenv");
+//const connectDB = require("./config/db");
+
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config({});
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
@@ -8,3 +18,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
 });
+
+// khrystynapeniak22
+// Dtk34Hp5Js0ijCtS
